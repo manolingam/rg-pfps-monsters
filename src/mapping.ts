cb4f3671d753061c7be4fdba8d5711b3094793d3
Manolingam N <manolingam1996@gmail.com>
2021-09-08 23:30:39 +0530

    updated schema with monster books

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,12 @@
+import { Address } from '@graphprotocol/graph-ts';
+
 import {
   Transfer,
   MonsterSpawn as MonsterContract
 } from '../generated/MonsterSpawn/MonsterSpawn';
-import { Mint, Monster, Owner, Trade } from '../generated/schema';
+import { MonsterBook as MonsterBookContract } from '../generated/MonsterBook/MonsterBook';
+
+import { Mint, Monster, Owner, Trade, MonsterBook } from '../generated/schema';
 
 const zeroAddress = '0x0000000000000000000000000000000000000000';
 
@@ -10,6 +14,7 @@ function handleClaim(event: Transfer): void {
   let mint = new Mint(event.transaction.hash.toHex());
 
   let monster = new Monster(event.params.tokenId.toHex());
+  let monsterBook = new MonsterBook(event.params.tokenId.toHex());
   let minter = Owner.load(event.params.to.toHex());
 
   if (minter == null) {
@@ -18,6 +23,32 @@ function handleClaim(event: Transfer): void {
 
   let monsterContract = MonsterContract.bind(event.address);
   let mintedTokenUri = monsterContract.tokenURI(event.params.tokenId);
+
+  let monsterBookContract = MonsterBookContract.bind(
+    Address.fromString('0x1B7c86617636856F3c95868490d23678a7445dfD')
+  );
+
+  let name = monsterBookContract.getName(event.params.tokenId);
+  let size = monsterBookContract.getSize(event.params.tokenId);
+  let weakness = monsterBookContract.getWeakness(event.params.tokenId);
+  let specialAbility = monsterBookContract.getSpecialAbility(
+    event.params.tokenId
+  );
+  let locomotion = monsterBookContract.getLocomotion(event.params.tokenId);
+  let language = monsterBookContract.getLanguage(event.params.tokenId);
+  let alignment = monsterBookContract.getAlignment(event.params.tokenId);
+  let action1 = monsterBookContract.getAction1(event.params.tokenId);
+  let action2 = monsterBookContract.getAction2(event.params.tokenId);
+
+  monsterBook.name = name;
+  monsterBook.size = size;
+  monsterBook.weakness = weakness;
+  monsterBook.specialAbility = specialAbility;
+  monsterBook.locomotion = locomotion;
+  monsterBook.language = language;
+  monsterBook.alignment = alignment;
+  monsterBook.action1 = action1;
+  monsterBook.action2 = action2;
 
   minter.address = event.params.to;
 
@@ -28,7 +59,9 @@ function handleClaim(event: Transfer): void {
   monster.currentOwner = minter.id;
   monster.tokenUri = mintedTokenUri;
   monster.mintInfo = mint.id;
+  monster.monsterInfo = monsterBook.id;
 
+  monsterBook.save();
   mint.save();
   minter.save();
   monster.save();
